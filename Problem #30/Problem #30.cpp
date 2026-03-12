5bea8e7239193edc93d813683bebd7aeabb6f5c2
Mohammed Najeh Al-zo'ubi <m7md.alzobi10@gmail.com>
2026-03-12 11:27:38 +0300

    Add files via upload

---
+++ Problem #30/Problem #30.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int ReadPositiveNumbers(string Message)
+{
+    int Number;
+
+    do
+    {
+        cout << Message;
+        cin >> Number;
+
+    } while (Number < 0);
+
+
+        return Number;
+}
+
+
+int Factorial(int N)
+{
+    int F = 1;
+
+    for (int Counter = N; Counter >= 1 ; Counter--)
+    {
+        F = Counter * F;
+
+    }
+
+    return F;
+}
+
+
+int main()
+{
+
+    cout << Factorial(ReadPositiveNumbers("Enter Number: ")) << endl;
+
+    return 0;
+}
